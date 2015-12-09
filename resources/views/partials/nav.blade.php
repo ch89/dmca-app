a87b439504d4ef171948df100adf3701ab72a6e3
ch89 <chhq89@gmail.com>
2015-12-09 01:24:20 +0100

    added home page

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,34 @@
+<nav class='navbar navbar-default navbar-static-top'>
+	<div class='container'>
+		<div class='navbar-header'>
+			<button class='navbar-toggle' data-toggle='collapse' data-target='#navbar'>
+				<span class='icon-bar'></span>
+				<span class='icon-bar'></span>
+				<span class='icon-bar'></span>
+			</button>
+			
+			<a href='/' class='navbar-brand'>DMCA Sender</a>
+		</div>
+
+		<div id='navbar' class='collapse navbar-collapse'>
+			<ul class='nav navbar-nav'>
+				<li><a href='/tasks'>Tasks</a></li>
+			</ul>
+
+			<ul class='nav navbar-nav navbar-right'>
+				@if(Auth::guest())
+					<li><a href='/auth/login'>Login</a></li>
+					<li><a href='/auth/register'>Register</a></li>
+				@else
+					<li class='dropdown'>
+						<a href='#' class='dropdown-toggle' data-toggle='dropdown'>{{ Auth::user()->name }} <span class='caret'></span></a>
+
+						<ul class='dropdown-menu'>
+							<li><a href='/auth/logout'>Logout</a></li>
+						</ul>
+					</li>
+				@endif
+			</ul>
+		</div>
+	</div>
+</nav>
