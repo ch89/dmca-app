1d6d85374cf3e61e4c2702447fd6c7b4bb8cbd17
ch89 <chhq89@gmail.com>
2015-12-10 01:02:37 +0100

    added authentication

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,23 @@
+@extends('app')
+
+@section('content')
+
+	<h1>Login</h1>
+
+	{!! Form::open() !!}
+		<div class='form-group'>
+			{!! Form::label('email', 'Email') !!}
+			{!! Form::text('email', null, ['class' => 'form-control']) !!}
+		</div>
+	
+		<div class='form-group'>
+			{!! Form::label('password', 'Password') !!}
+			{!! Form::password('password', ['class' => 'form-control']) !!}
+		</div>
+	
+		<div class='form-group'>
+			{!! Form::submit('Login', ['class' => 'btn btn-primary']) !!}
+		</div>
+	{!! Form::close() !!}
+
+@stop
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,33 @@
+@extends('app')
+
+@section('content')
+
+	<h1>Register</h1>
+
+	{!! Form::open() !!}
+		<div class='form-group'>
+			{!! Form::label('name', 'Name') !!}
+			{!! Form::text('name', null, ['class' => 'form-control']) !!}
+		</div>
+	
+		<div class='form-group'>
+			{!! Form::label('email', 'Email') !!}
+			{!! Form::text('email', null, ['class' => 'form-control']) !!}
+		</div>
+	
+		<div class='form-group'>
+			{!! Form::label('password', 'Password') !!}
+			{!! Form::password('password', ['class' => 'form-control']) !!}
+		</div>
+	
+		<div class='form-group'>
+			{!! Form::label('password_confirmation', 'Confirm Password') !!}
+			{!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
+		</div>
+	
+		<div class='form-group'>
+			{!! Form::submit('Register', ['class' => 'btn btn-primary']) !!}
+		</div>
+	{!! Form::close() !!}
+
+@stop
